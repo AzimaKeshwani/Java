8121f02cc102dd6a71f58d9accab17382daf8386
Azima <azima786@gmail.com>
2024-09-29 05:39:13 -0500

    programs

--- src/test/java/Strings.java
+++ src/test/java/Strings.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class Strings {
     public static void main(String[] args) {
-        String a = "  Welcome to Dallas!  ";
+        String a = "  Welcome to programming!  ";
         String abc = "Azima@gmail.com";
         String numbers = "$123, 124, 543";
         String welcome = "Welcome";
@@ -12,38 +12,39 @@ public class Strings {
         String[] number = (numbers.split(","));
         System.out.println(numbers);
         System.out.println(number[0]);
-        System.out.println(removeSpacesFromString(a));
+        System.out.println(trimString(a));
         System.out.println(containsWord(a, "Dallas"));
         System.out.println(replaceValue(a, "Dallas", "America"));
-        System.out.println(extractSubString(a));
+        System.out.println(extractSubString(a, 2));
         System.out.println(Arrays.toString(splitString(abc, "@")));
         System.out.println(reverseString(welcome));
         System.out.println(reverseStringUsingStringBuilder(welcome));
-        System.out.println(isStringPalindrome(p, p));
+        System.out.println(isStringPalindrome(p));
         System.out.println(removeSpecialCharacters(special));
         System.out.println(removeWhiteSpaces(a));
         System.out.println(countOccurencesOfCharacter(a, 'w'));
+        System.out.println(countWordsInString(a));
     }
 
-    public static String removeSpacesFromString(String a) {
-        a = a.trim();
-        return a;
+    public static String trimString(String a) {
+        return a.trim();
     }
 
     public static boolean containsWord(String a, String elementToCheck) {
-        boolean containsValue = false;
-        containsValue = a.contains(elementToCheck);
 
-        return containsValue;
+        return a.contains(elementToCheck);
+
     }
 
     public static String replaceValue(String a, String oldString, String newString) {
-        a = a.replaceAll(oldString, newString);
-        return a;
+        return a.replaceAll(oldString, newString);
     }
 
-    public static String extractSubString(String a) {
-        a = a.substring(2, a.length() - 1).toLowerCase();
+    public static String extractSubString(String a, int starting) {
+        if (starting < 0 || starting >= a.length()) {
+            return "";
+        }
+        a = a.substring(starting).toLowerCase();
         return a;
     }
 
@@ -61,15 +62,13 @@ public class Strings {
         return rev;
     }
 
-    public static StringBuilder reverseStringUsingStringBuilder(String reverse) {
-        StringBuilder stringBuilder = new StringBuilder(reverse);
-        stringBuilder.reverse();
-        return stringBuilder;
+    public static String reverseStringUsingStringBuilder(String reverse) {
+        return new StringBuilder(reverse).reverse().toString();
     }
 
-    public static boolean isStringPalindrome(String a, String b) {
-        reverseString(a);
-        return a.equalsIgnoreCase(b);
+    public static boolean isStringPalindrome(String a) {
+        String reversed = reverseString(a);
+        return reversed.equalsIgnoreCase(a);
 
     }
 
@@ -91,6 +90,11 @@ public class Strings {
             }
         }
         return count;
+    }
+
+    public static int countWordsInString(String a) {
+        return a.trim().split("\\s+").length;
+
     }
 
 }
